b2aa5a4c3e0e5f6e15f30d5aca5e646037774a12
tito300 <tarek.demachkie@gmail.com>
2018-03-25 02:56:28 -0500

    Added totalItems function

--- routs/cart.js
+++ routs/cart.js
@@ -4,13 +4,22 @@ const User = require('../models/user');
 const Product = require("../models/product");
 const Convert = require("../seed/db-to-cart.js")
  
+function calcTotals(array){
+    // debugger;
+    let total = 0;
+    array.forEach((c) => {
+        total += c.total;
+    });
+    return total
+}
+
 
 router.get("/cart/add/:id", (req, res)=>{
     // debugger;
 
     // pull product information -> create a cart item object -> push to cart -> calc values
 
-    // 1- pull product information
+    // 1-find and pull item
     let productID = req.params.id;
     Product.findOne({id: productID})
             .then((data)=> {
@@ -19,23 +28,53 @@ router.get("/cart/add/:id", (req, res)=>{
                 let newObj = new Convert(data, req);
                 let objID = newObj.id;
                 
-                //  to find whether item has been added before or not
-                User.findOne({"cart.items.id": objID}).then((data)=>{
-                  if(!data) {
-                    User.update({ googleID: req.user.googleID}, {$push: {"cart.items": newObj}}, ()=> {console.log("new item added to cart!")});
-                  }  else if (data){
-                    // debugger;  
-                    
-                    // find index of item to pull index which will be used to update total - 
-                    let index = data.cart.items.findIndex((c)=>{ return c.id === objID})  
-                    let total = data.cart.items[index].total;
-                    total++  
-                //  find sub object in array by id -> use $ to refer to object found in array and then update value
-                    User.update({ "cart.items.id": objID}, {$set: {"cart.items.$.total": total}}, ()=> {console.log("total update done!")});
-                    total = 1;
-                  }
-                    
-                })
+                //  2- find whether item has been added before or not
+                User.findOne({"cart.items.id": objID})
+                    .then((data)=>{
+
+                        // 3- add item/total
+                        if(!data) {
+                            User.update({ googleID: req.user.googleID}, {$push: {"cart.items": newObj}}, ()=> {console.log("new item added to cart!")})
+                                .then(()=>{
+
+                                    User.findOne({googleID: req.user.googleID})
+                                    .then((data)=> {
+                                        debugger;
+                                        let totalItems = calcTotals(data.cart.items);
+                                        data.cart.totalItems = totalItems;
+                                        data.save();
+                                        console.log(totalItems);
+
+                                    })
+                                });
+
+                        }  else if (data){
+                            // debugger;  
+                            
+                            // find index of item to pull index which will be used to update total - 
+                            let index = data.cart.items.findIndex((c)=>{ return c.id === objID})  
+                            let total = data.cart.items[index].total;
+                            total++  
+                            //  find sub object in array by id -> use $ to refer to object found in array and then update value
+                            User.update({ "cart.items.id": objID}, {$set: {"cart.items.$.total": total}}, ()=> {console.log("total update done!")})
+                            .then(()=>{
+
+                                User.findOne({googleID: req.user.googleID})
+                                .then((data)=> {
+                                    debugger;
+                                    let totalItems = calcTotals(data.cart.items);
+                                    data.cart.totalItems = totalItems;
+                                    data.save();
+                                    console.log(totalItems);
+                                })
+                            });
+
+                            total = 1;
+                        }
+
+                        
+                            
+                        })
                 
             });
     
