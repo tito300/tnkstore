ccbbbbbde910a96c118e5227d1909ffd818434ad
tito300 <tarek.demachkie@gmail.com>
2018-11-02 17:24:08 -0500

    refactored cart server to use async/await

--- routs/cart.js
+++ routs/cart.js
@@ -7,131 +7,118 @@ const User = require('../models/user');
 const Product = require('../models/product');
 const util = require('../util/util.js');
 
-
-router.get('/cart/add/:id', (req, res) => {
-  // pull product information -> create a cart item object -> push to cart -> calc values
-
+/* * *
+ *
+ * Route to add new item to cart.
+ * pulls product information -> create a cart item object -> push to cart -> calc total value
+ *
+ * * * * */
+router.get('/cart/add/:id', async (req, res) => {
   // 1-find and pull item
   const productID = req.params.id;
-  Product.findOne({ id: productID })
-    .then((data) => {
-      const newObj = new util.Convert(data);
-      const objID = newObj.id;
+  const item = await Product.findOne({ id: productID });
+  const newObj = new util.Convert(item);
+  const objID = newObj.id;
 
-      //  2- find whether item has been added before or not
-      User.findOne({ 'cart.items.id': objID })
-        .then((data) => {
-          // 3- add item/total
-          if (!data) {
-            User.update({ googleID: req.user.googleID }, { $addToSet: { 'cart.items': newObj } }, () => { console.log('new item added to cart!'); })
-              .then(() => {
-                User.findOne({ googleID: req.user.googleID })
+  //  2- find whether item has been added before or not
+  const itemExist = await User.findOne({ 'cart.items.id': objID });
+  // 3- add item/total
+  if (!itemExist) {
+    await User.update({ googleID: req.user.googleID }, { $addToSet: { 'cart.items': newObj } }, () => { console.log('new item added to cart!'); });
+    const user = await User.findOne({ googleID: req.user.googleID });
 
-                // calc totalItems
-                  .then((data) => {
-                    const totalItems = util.calcTotals(data.cart.items);
-                    data.cart.totalItems = totalItems.items;
-                    data.cart.totalPrice = totalItems.price;
-                    data.save();
-                    // console.log(totalItems);
-                    // res.redirect("/top-sellers");
-                    // res.end();
-                    const body = {
-                      total: totalItems.items,
-                    };
-                    res.send(JSON.stringify(body));
-                  });
-              });
-          } else if (data) {
-            // find index of item to pull index which will be used to update total -
-            const index = data.cart.items.findIndex(c => c.id === objID);
-            let total = data.cart.items[index].total; //eslint-disable-line
-            total++;
-            //  find sub object in array by id -> use $ to refer to object
-            //  found in array and then update value
-            User.update({ 'cart.items.id': objID }, { $set: { 'cart.items.$.total': total } }, () => { console.log('total update done!'); })
-              .then(() => {
-                User.findOne({ googleID: req.user.googleID })
+    // calc totalItems
+    const totalItems = util.calcTotals(user.cart.items);
+    user.cart.totalItems = totalItems.items;
+    user.cart.totalPrice = totalItems.price;
+    await user.save();
+    const body = {
+      total: totalItems.items,
+    };
+    res.send(JSON.stringify(body));
+  } else if (itemExist) {
+    // find index of item to pull index which will be used to update total -
+    const index = itemExist.cart.items.findIndex(c => c.id === objID);
+    let total = itemExist.cart.items[index].total; //eslint-disable-line
+    total++;
+    //  find sub object in array by id -> use $ to refer to object
+    //  found in array and then update value
+    await User.update({ 'cart.items.id': objID }, { $set: { 'cart.items.$.total': total } }, () => { console.log('total update done!'); });
 
-                // calc totalItems
-                  .then((data) => {
-                    const totalItems = util.calcTotals(data.cart.items);
-                    data.cart.totalItems = totalItems.items;
-                    data.cart.totalPrice = totalItems.price;
-                    data.save();
-                    // console.log(totalItems);
-                    // res.redirect("/top-sellers");
-                    const body = {
-                      total: totalItems.items,
-                    };
-                    res.send(JSON.stringify(body));
-                  });
-              });
+    const user = await User.findOne({ googleID: req.user.googleID });
 
-            total = 1;
-          }
-        });
-    });
-
-
-  // res.redirect("/top-sellers");
+    // calc totalItems
+    const totalItems = util.calcTotals(user.cart.items);
+    user.cart.totalItems = totalItems.items;
+    user.cart.totalPrice = totalItems.price;
+    await user.save();
+    const body = {
+      total: totalItems.items,
+    };
+    res.send(JSON.stringify(body));
+    total = 1;
+  }
 });
 
-
+/* * *
+ *
+ * main page redirect
+ *
+ * * * * */
 router.get('/cart/main', (req, res, next) => {
   res.render('cart/cartPage', { 'user': req.user });
 });
 
-
-router.put('/cart/update-qty', (req, res) => {
-  // let itemQty = parseInt(req.body.itemsQty);
+/* * *
+ *
+ * updates cart items count when client changes it and re calculates total count.
+ *
+ * * * * */
+router.put('/cart/update-qty', async (req, res) => {
   const itemID = String(req.body.itemID);
-  // User.find({"cart.items[0].id": req.body.itemID}).then((userfound)=> console.log(userfound));
-  User.update({ 'cart.items.id': itemID }, { $set: { 'cart.items.$.total': req.body.itemsQty } })
-    .then(() => {
-      User.findOne({ googleID: req.user.googleID })
-
-        // calc totalItems
-        .then((data) => {
-          const totalItems = util.calcTotals(data.cart.items);
-          data.cart.totalItems = totalItems.items;
-          data.cart.totalPrice = totalItems.price;
-          data.save();
-
-          const totalsFile = {
-            totalItems: totalItems.items,
-          };
-
-          res.send(JSON.stringify(totalsFile));
-          // console.log(totalItems);
-        }).catch((err) => {
-          console.log(err);
-        });
-    });
+  await User.update({ 'cart.items.id': itemID }, { $set: { 'cart.items.$.total': req.body.itemsQty } });
+  try {
+    const user = await User.findOne({ googleID: req.user.googleID });
+    // calc totalItems
+    const totalItems = util.calcTotals(user.cart.items);
+    user.cart.totalItems = totalItems.items;
+    user.cart.totalPrice = totalItems.price;
+    await user.save();
+    const totalsFile = {
+      totalItems: totalItems.items,
+    };
+    res.send(JSON.stringify(totalsFile));
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-router.delete('/cart/delete/:id', (req, res) => {
+/* * *
+ *
+ * deletes item from cart and re-calculates total count.
+ *
+ * * * * */
+router.delete('/cart/delete/:id', async (req, res) => {
   const itemID = req.params.id;
 
-  User.update({ googleID: req.user.googleID }, { $pull: { 'cart.items': { 'id': req.params.id } } }).then(() => {
-    User.findOne({ googleID: req.user.googleID })
+  await User.update({ googleID: req.user.googleID }, { $pull: { 'cart.items': { 'id': req.params.id } } });
+  try {
+    const user = await User.findOne({ googleID: req.user.googleID });
     // calc totalItems
-      .then((data) => {
-        const totalItems = util.calcTotals(data.cart.items);
-        data.cart.totalItems = totalItems.items;
-        data.cart.totalPrice = totalItems.price;
-        data.save();
+    const totalItems = util.calcTotals(user.cart.items);
+    user.cart.totalItems = totalItems.items;
+    user.cart.totalPrice = totalItems.price;
+    await user.save();
 
-        const totalsFile = {
-          totalItems: totalItems.items,
-        };
+    const totalsFile = {
+      totalItems: totalItems.items,
+    };
 
-        res.send(JSON.stringify(totalsFile));
-        // console.log(totalItems);
-      }).catch((err) => {
-        console.log(err);
-      });
-  });
+    res.send(JSON.stringify(totalsFile));
+    // console.log(totalItems);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
