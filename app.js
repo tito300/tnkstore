0faa3c0b82b290c3821944b86d4ea795bc3afa58
tito300 <tarek.demachkie@gmail.com>
2018-11-06 00:48:38 -0600

    extracted server from app file to bin folder

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const createError = require('http-errors');
 const session = require('cookie-session');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const config = require('config');
 const aothRouter = require('./routs/aoth-routs.js');
 const mainRouter = require('./routs/main-routs.js');
 const productsRouter = require('./products/productsRouting.js');
@@ -11,13 +12,10 @@ const googleSetup = require('./passport-conf/google');
 
 
 const app = express();
-mongoose.connect('mongodb://localhost/fullstack3')
-  .then(() => console.log('connected successfully to db'));
-
 
 app.use(session({
   maxAge: 24 * 60 * 60 * 1000,
-  keys: ['tarekdemachkie'],
+  keys: [config.get('key')],
 }));
 
 app.use(bodyParser.json());
@@ -32,11 +30,16 @@ app.use('/aoth', aothRouter);
 app.use('/products', productsRouter);
 app.use('/', usersRouter);
 
+// handles 404 errors. these errors are not caused by an actual error
+// they indicate that the req reached this last middleware without a response
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(err.status || 500);
   res.send(err.message);
 });
 
-
-app.listen(3000, () => { console.log('listening on port 3000'); });
+module.exports = { app };
